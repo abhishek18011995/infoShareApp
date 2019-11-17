997129b7be0319d61b526615b84440c96276ac05
Kumar Abhishek <abhishek18011995@gmail.com>
2019-11-18 01:00:02 +0530

    Api solution added

---
+++ InfoShareApp.API.Application/InfoShareApp.API.Application/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using InfoShareApp.API.Application.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using InfoShareApp.API.Common.Services.Logging;
+
+namespace InfoShareApp.API.Application
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateWebHostBuilder(args).Build();
+            
+            try
+            {
+                host.Run();
+            } catch(Exception ex)
+            {
+                var logger = host.Services.GetRequiredService<ILogger<Program>>();
+                logger.LogCritical(LoggingEvents.AppStartFailure, ex, "Error occured in Main");
+            }
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .ConfigureLogging()
+                .UseStartup<Startup>();
+    }
+}
---
+++ InfoShareApp.API.Application/InfoShareApp.API.Common/Services/Logging/LoggingEvents.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InfoShareApp.API.Common.Services.Logging
+{
+    public class LoggingEvents
+    {
+        public const int AppStartFailure = 1001;
+    }
+}
