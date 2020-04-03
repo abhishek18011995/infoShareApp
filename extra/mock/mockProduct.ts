31dec7a04a7056776bff659db7e354a4bb6a7179
Kumar Abhishek <212752788@apiazuredevge.onmicrosoft.com>
2020-04-03 12:49:53 +0530

    client app added

--- extra/mock/mockProduct.ts
+++ extra/mock/mockProduct.ts
@@ -2,62 +2,62 @@ import { Gender } from "../models/enums";
 import { IProduct } from "../models/product";
 
 const products: IProduct[] = [{
-    id: '11',
-    name: 'Chromozome Sando',
-    description: 'Chromozome Sando',
-    imgPath: 'string',
-    type: 'innerwear',
+    id: "11",
+    name: "Chromozome Sando",
+    description: "Chromozome Sando",
+    imgPath: "string",
+    type: "innerwear",
     inStock: true,
     price: {
         mrp: 300,
         discountPrice: 250
     },
-    brandName: 'Chromozome',
+    brandName: "Chromozome",
     gender: Gender.MALE,
     color: "white"
 },
 {
-    id: '12',
-    name: 'Killer jeans 12',
-    description: 'Killer jeans 12',
-    imgPath: 'string',
-    type: 'Jeans',
+    id: "12",
+    name: "Killer jeans 12",
+    description: "Killer jeans 12",
+    imgPath: "string",
+    type: "Jeans",
     price: {
         mrp: 2499,
         discountPrice: 1199
     },
     inStock: true,
-    brandName: 'Killer',
+    brandName: "Killer",
     gender: Gender.MALE,
-    color: 'black'
+    color: "black"
 },
 {
-    id: '12',
-    name: 'jeans 12',
-    description: 'Levis jeans 12',
-    imgPath: 'string',
-    type: 'Jeans',
+    id: "12",
+    name: "jeans 12",
+    description: "Levis jeans 12",
+    imgPath: "string",
+    type: "Jeans",
     price: {
         mrp: 2499,
         discountPrice: 1199
     },
     inStock: true,
-    brandName: 'Levis',
+    brandName: "Levis",
     gender: Gender.MALE,
-    color: 'black'
+    color: "black"
 },
 {
-    id: '113',
-    name: 'blue Jacket',
-    description: 'Celio Blue Jacket',
+    id: "113",
+    name: "blue Jacket",
+    description: "Celio Blue Jacket",
     price: {
         mrp: 2499,
         discountPrice: 1199
     },
-    imgPath: 'string',
-    type: 'Jacket',
+    imgPath: "string",
+    type: "Jacket",
     inStock: true,
-    brandName: 'Celio',
+    brandName: "Celio",
     gender: Gender.FEMALE,
-    color: 'blue'
+    color: "blue"
 }]
